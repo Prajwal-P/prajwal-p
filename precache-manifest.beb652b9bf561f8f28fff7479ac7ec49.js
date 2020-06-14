47ef56dac38ce0927a22d22d06fc79bfbbdfb843
Prajwal.P <50076923+Prajwal-P@users.noreply.github.com>
2020-06-14 20:19:24 +0530

    Updates

---
+++ precache-manifest.beb652b9bf561f8f28fff7479ac7ec49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af946253a44a5aab5ad231c3ddb271ef",
+    "url": "/prajwal-p/index.html"
+  },
+  {
+    "revision": "d57e059ffe275e8ce3c2",
+    "url": "/prajwal-p/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c2499bb057414b3c5df9",
+    "url": "/prajwal-p/static/js/2.bc373a11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prajwal-p/static/js/2.bc373a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d57e059ffe275e8ce3c2",
+    "url": "/prajwal-p/static/js/main.fa91a28d.chunk.js"
+  },
+  {
+    "revision": "25ec332086ded1f431bb",
+    "url": "/prajwal-p/static/js/runtime-main.79a8ab4d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/prajwal-p/static/media/logo.5d5d9eef.svg"
+  }
+]);
