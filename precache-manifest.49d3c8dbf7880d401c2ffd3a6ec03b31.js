8da615127894db47648c7b5f6ab54a7eaef8d510
Prajwal.P <50076923+Prajwal-P@users.noreply.github.com>
2020-06-17 15:52:38 +0530

    Updates

--- precache-manifest.49d3c8dbf7880d401c2ffd3a6ec03b31.js
+++ precache-manifest.49d3c8dbf7880d401c2ffd3a6ec03b31.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1275d5042862e17988ca269ae4408ba1",
+    "revision": "2a78fb4eb526c617471d9d296cb2d6e2",
     "url": "/prajwal-p/index.html"
   },
   {
-    "revision": "fc717e5d66d6b308a52e",
+    "revision": "62930630935299f44d95",
     "url": "/prajwal-p/static/css/main.df24737b.chunk.css"
   },
   {
-    "revision": "ab0cd9c7c893ca9dbcdd",
-    "url": "/prajwal-p/static/js/2.54eb7557.chunk.js"
+    "revision": "08a14f5f4c53a0decd7e",
+    "url": "/prajwal-p/static/js/2.be749039.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/prajwal-p/static/js/2.54eb7557.chunk.js.LICENSE.txt"
+    "url": "/prajwal-p/static/js/2.be749039.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fc717e5d66d6b308a52e",
-    "url": "/prajwal-p/static/js/main.67edd5fc.chunk.js"
+    "revision": "62930630935299f44d95",
+    "url": "/prajwal-p/static/js/main.675ab052.chunk.js"
   },
   {
     "revision": "25ec332086ded1f431bb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/prajwal-p/precache-manifest.a9738e15ccb1e1820a34d393fcbb2e12.js"
+  "/prajwal-p/precache-manifest.49d3c8dbf7880d401c2ffd3a6ec03b31.js"
 );
 
 self.addEventListener('message', (event) => {
