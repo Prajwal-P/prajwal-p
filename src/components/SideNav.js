c700a632ae9b94d0ba607241e8fea3e413bff159
Prajwal.P <50076923+Prajwal-P@users.noreply.github.com>
2020-06-22 06:47:57 +0530

    menu for mobile

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -8,50 +8,82 @@ import profile_pic from '../Assets/profile-pic-2.jpg'
 
 
 class SideNav extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			showMenu: false
+		};
 	}
 
-	render(){
-		return(
+	showMenu = () => {
+		// console.log('Test')
+		const menuBtn = document.querySelector('.menu-btn');
+		const profile = document.querySelector('.profile');
+		const menuNav = document.querySelector('.menu-nav');
+		const sideNav = document.querySelector('.side-nav');
+
+		if(!this.state.showMenu){
+			menuBtn.classList.add('close');
+			profile.classList.add('show');
+			menuNav.classList.add('show');
+			sideNav.classList.add('show');
+			
+			this.setState({showMenu: true})
+		} else {
+			menuBtn.classList.remove('close');
+			profile.classList.remove('show');
+			menuNav.classList.remove('show');
+			sideNav.classList.remove('show');
+			
+			this.setState({showMenu: false})
+		}
+	}
+	
+	closeMenu = () => {
+		this.setState({showMenu: false})
+	}
+
+	render() {
+		return (
 			<React.Fragment>
 				<HashRouter basename='/'>
 					<div className='app-wrapper'>
-						<div class="menu-btn">
-							<div class="btn-line"></div>
-							<div class="btn-line"></div>
-							<div class="btn-line"></div>
-						</div>
 						<div className="side-nav">
+							<div className="menu-btn" onClick={this.showMenu}>
+								<div className="btn-line"></div>
+								<div className="btn-line"></div>
+								<div className="btn-line"></div>
+							</div>
 							<div className="profile">
-								<img alt='My Pic' src={profile_pic}/>
+								<img alt='My Pic' src={profile_pic} />
 								<h1>Prajwal P</h1>
 							</div>
-							<RoutedTabs activeTabClassName='active' className='links'>
-								<NavTab to='/home' className='link-item'>
-									<div><i className="fas fa-home"></i> Home</div>
-								</NavTab>
-								<NavTab to='/project' className='link-item'>
-									<div><i className="fas fa-briefcase"></i> Project</div>
-								</NavTab>
-								<NavTab to='/contact' className='link-item'>
-									<div><i className="far fa-envelope-open"></i> Contact</div>
-								</NavTab>
-							</RoutedTabs>
-							<div className="collection">
-								<a href='https://github.com/Prajwal-P' target='_blank' rel="noopener noreferrer">
-									<i className="fab fa-github fa-2x"></i>
-								</a>
-								<a href='https://www.linkedin.com/in/prajwal-p-21a9ba172/' target='_blank' rel="noopener noreferrer">
-									<i className="fab fa-linkedin-in fa-2x"></i>
-								</a>
-								<a href='https://www.hackerrank.com/prajwal72p' target='_blank' rel="noopener noreferrer">
-									<i className="fab fa-hackerrank fa-2x"></i>
-								</a>
-								<a href='https://www.youracclaim.com/users/prajwal/badges' target='_blank' rel="noopener noreferrer">
-									<i className="fas fa-medal fa-2x"></i>
-								</a>
+							<div className="menu-nav">
+								<RoutedTabs activeTabClassName='active' className='links' onClick={this.showMenu}>
+									<NavTab to='/home' className='link-item'>
+										<div><i className="fas fa-home"></i> Home</div>
+									</NavTab>
+									<NavTab to='/project' className='link-item'>
+										<div><i className="fas fa-briefcase"></i> Project</div>
+									</NavTab>
+									<NavTab to='/contact' className='link-item'>
+										<div><i className="far fa-envelope-open"></i> Contact</div>
+									</NavTab>
+								</RoutedTabs>
+								<div className="collection">
+									<a href='https://github.com/Prajwal-P' target='_blank' rel="noopener noreferrer">
+										<i className="fab fa-github fa-2x"></i>
+									</a>
+									<a href='https://www.linkedin.com/in/prajwal-p-21a9ba172/' target='_blank' rel="noopener noreferrer">
+										<i className="fab fa-linkedin-in fa-2x"></i>
+									</a>
+									<a href='https://www.hackerrank.com/prajwal72p' target='_blank' rel="noopener noreferrer">
+										<i className="fab fa-hackerrank fa-2x"></i>
+									</a>
+									<a href='https://www.youracclaim.com/users/prajwal/badges' target='_blank' rel="noopener noreferrer">
+										<i className="fas fa-medal fa-2x"></i>
+									</a>
+								</div>
 							</div>
 						</div>
 						<div className='main'>
