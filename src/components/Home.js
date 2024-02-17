db7d1c488718f7e0338eb862d186dcf01d347889
Prajwal.P <50076923+Prajwal-P@users.noreply.github.com>
2024-02-17 12:29:24 +0530

    rearranged home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,19 +12,15 @@ export default class Home extends Component {
 		return (
 			<React.Fragment>
 				<div className='division'>
-					<h2 className='underline'>Education</h2>
-					<Card name='Bangalore Institute of Technology'
-						time='2021'
-						marks='7.91 CGPA'
-						type='B. E, Computer Science' />
-					<Card name='Karkala JnanaSudha P.U College'
-						time='2017'
-						marks='Class XII 89.6%'
-						type='Secondary High school, PCMC' />
-					<Card name='M.E.S Kishora Kendra'
-						time='2015'
-						marks='Class X 91.2%'
-						type='High School' />
+					<h1>Hi there 👋️</h1>
+				</div>
+				<div className='division'>
+					<h2 className='underline'>Experience</h2>
+					<h3>Papple Technologies</h3>
+					<ul>
+						<li>Developed a web app where students can attend quiz and prepare for exams</li>
+						<li><span className='underline bold'>Tools:</span> Reactjs, Nodejs, MySQL, Visual Studio Code</li>
+					</ul>
 				</div>
 				<div className='division'>
 					<h2 className='underline'>Skills</h2>
@@ -42,12 +38,19 @@ export default class Home extends Component {
 					</div>
 				</div>
 				<div className='division'>
-					<h2 className='underline'>Experience</h2>
-					<h3>Papple Technologies</h3>
-					<ul>
-						<li>Developed a web app where students can attend quiz and prepare for exams</li>
-						<li><span className='underline bold'>Tools:</span> Reactjs, Nodejs, MySQL, Visual Studio Code</li>
-					</ul>
+					<h2 className='underline'>Education</h2>
+					<Card name='Bangalore Institute of Technology'
+						time='2021'
+						marks='7.91 CGPA'
+						type='B. E, Computer Science' />
+					<Card name='Karkala JnanaSudha P.U College'
+						time='2017'
+						marks='Class XII 89.6%'
+						type='Secondary High school, PCMC' />
+					<Card name='M.E.S Kishora Kendra'
+						time='2015'
+						marks='Class X 91.2%'
+						type='High School' />
 				</div>
 			</React.Fragment>
 		)
